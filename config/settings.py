5b772d2928449af02e4f7b88aec5100c93971efa
sadeeq <sadeejert@gmail.com>
2026-04-17 21:11:46 +0100

    fix trailing slash bug

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from decouple import config, Csv
 import dj_database_url
 
+APPEND_SLASH=False
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 
 # Application definition
